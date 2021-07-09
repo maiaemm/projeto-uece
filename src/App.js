0c1dddfc1f8ec8da19812ab6336303dc0231ff6a
emilly <emilly.pereira.maia08@aluno.ifce.edu.br>
2021-07-08 23:45:19 -0300

    iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from './Pages/Login/Login.js';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './style.css'
+
+function Login() {
+  const [email, setEmail] = useState('Milshake');
+  const [senha, setSenha] = useState(0);
+
+  const handleChange = (evt) => {
+    setEmail(evt.target.value)
+  }
+
+  const handlePassword = (evt) => {
+    setSenha(evt.target.value)
+  }
+
+  return (
+    <div className="login-container">
+      <h3>Olá! {email}</h3>
+      <div>
+        <img src="https://i.pinimg.com/originals/66/c0/f3/66c0f3951cf18634e632c383284ab1f7.png" alt="imagem do usuário" />
+      </div>
+      <label htmlFor='email-user'>Email: 
+        <input type="text" id='email-user' onChange={ handleChange } />
+      </label>
+      <div>
+      <label htmlFor="password-user">Senha:
+        <input type="password" id="password-user" onChange={ handlePassword }/>
+      </label>
+      </div>
+      <p>
+        Não tem conta ? Crie agora.
+      </p>
+      <p>Sua senha é: {senha}</p>
+    </div>
+  );
+}
+
+export default Login;
