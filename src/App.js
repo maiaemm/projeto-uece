ebba16f139a9febcb588ba9fcf34cded742ed886
brunobastosfer <brunobastos@edu.unifor.br>
2021-07-10 19:12:29 -0300

    ajustando as telas e colocando rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login/Login.js';
+import Register from './Pages/Register/Register.js';
+import Forget from './Pages/Forget/Forget.js';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/register" exact component={ Register } />
+        <Route path="/forget" exact component={ Forget } />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Pages/Register/Register.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+import '../Register/Register.css';
+
+function Register() {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [lastName, setlastName] = useState('')
+
+  const handleChangeEmail = ({ target: { value } }) => {
+    setEmail(value)
+  }
+
+  const handleChangeName = ({ target: { value } }) => {
+    setName(value)
+  }
+
+  const handleChangeLastName = ({ target: { value } }) => {
+    setlastName(value)
+  }
+
+  const handleClick = () => console.log(email, name, lastName)
+
+  return (
+    <div id="main-container">
+      <h1>Cadastre-se para acessar o sistema</h1>
+      <form id="register-form" action="">
+        <div class="full-box">
+          <label for="email">E-mail</label>
+          <input onChange={ handleChangeEmail } type="email" name="email" id="email" placeholder="seuemail@seuemail.com" data-min-length="2" data-email-validate />
+        </div>
+        <div class="half-box spacing">
+            <label for="name">Nome</label>
+            <input onChange={ handleChangeName } type="text" name="name" id="name" placeholder="Seu nome" data-required data-min-length="3" data-max-length="16"/>
+        </div>
+        <div class="half-box">
+            <label for="lastname">Sobrenome</label>
+            <input onChange={ handleChangeLastName } type="text" name="lastname" id="lastname" placeholder="Seu sobrenome" data-required data-only-letters />
+        </div>
+        <div class="half-box spacing">
+          <label for="lastname">Senha</label>
+          <input type="password" name="password" id="password" placeholder="Sua senha" data-password-validate data-required />
+        </div>
+        <div class="half-box">
+          <label for="passconfirmation">Confirmação de senha</label>
+          <input type="password" name="passconfirmation" id="passwordconfirmation" placeholder="Digite novamente" data-equal="password" />
+        </div>
+        <div>
+          <input type="checkbox" name="agreement" id="agreement" />
+          <label for="agreement" id="agreement-label">Eu li e aceito os
+            <Link to=""> termos de uso</Link>
+          </label>
+        </div>
+        <div class="full-box">
+          <button id="btn-submit" type="button" onClick={ handleClick }>Registrar</button>
+        </div>
+      </form>
+  </div>
+  );
+}
+
+export default Register;
