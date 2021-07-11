2488bf0df41349d76ce999a2c8ee2389305bf26c
emilly <emilly.pereira.maia08@aluno.ifce.edu.br>
2021-07-11 02:46:07 -0300

    primeiro ajuste

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -16,13 +16,13 @@ function Login() {
       <h1>Login </h1>
       <img className="bobinho" src="https://darlildo.files.wordpress.com/2014/08/regular-show-about-the-show.png" alt="foto-bobinho" />
       <form action="">
-        <label for="email">E-mail</label>
-        <input  onChange={ handleChange } type="email" name="email" id="email" placeholder="Digite seu e-email" autocomplete="off" />
-        <label for="password">Senha</label>
-        <input onChange={ handleChange2 } type="password" name="password" id="password" placeholder="Digite sua senha" />
-        <Link to="/forget" id="forgot-pass">Esqueceu a senha?</Link>
+        <label className='describes' for="email">E-mail</label>
+        <input className='input-form' onChange={ handleChange } type="email" name="email" id="email" placeholder="Digite seu e-email" autocomplete="off" />
+        <label  className='describes'for="password">Senha</label>
+        <input className='input-form' onChange={ handleChange2 } type="password" name="password" id="password" placeholder="Digite sua senha" />
+        <Link className='clicaveis' to="/forget" id="forgot-pass">Esqueceu a senha?</Link>
           <Link to="/user" id="user-link">
-            <button onClick={ handleClick }>Login</button>
+            <button className='bot' onClick={ handleClick }>Login</button>
           </Link>
       </form>
     <div id="register-container">
